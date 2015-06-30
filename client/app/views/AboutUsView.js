e41a33ecb98a5ffd4d058bb54f8ec5f81a2d5a4e
Francois Romain <francoisromain@gmail.com>
2015-06-30 16:54:11 -0700

    (refactor) replace bootstrap with semantic

--- client/app/views/AboutUsView.js
+++ client/app/views/AboutUsView.js
@@ -8,34 +8,6 @@ var AboutUsView = Backbone.View.extend({
         <div class="col-md-10 col-md-offset-1 text-center" id="about-us-container">\
           <div class="row"><h2>About Us</h2></div>\
           <div class="member-profiles row text-left">\
-            <div class="member-profile col-xs-4" id="edwin-profile"> \
-                <img src="assets/images/Edwin.png" class="profile-image"> \
-                <div class="text-center row profile-name">Edwin Lin</div>\
-                <div class="text-center row"><ul class="nav navbar navbar-nav about-us-nav">\
-                  <li><a href="https://github.com/edwinlin1987" target="_blank"><i class="glyphicon icon-large icon-search icon-github"></i></a></li>\
-                  <li><a href="https://www.linkedin.com/in/edwinlin1987" target="_blank"><i class="glyphicon icon-large icon-search icon-linked-in linkedin-icon"></i></a></li>\
-                  <li><a href="http://eddyjs.com" target="_blank"><i class="glyphicon icon-large icon-search icon-blog"></i></a></li>\
-                </ul></div>\
-              </ul>\
-            </div> \
-            <div class="member-profile col-xs-4" id="paul-profile"> \
-                <img src="assets/images/Paul.png" class="profile-image"> \
-                <div class="text-center row profile-name">Paul Sokolik</div>\
-                <div class="text-center row"><ul class="nav navbar navbar-nav about-us-nav">\
-                  <li><a href="https://github.com/sokolikp" target="_blank"><i class="glyphicon icon-large icon-search icon-github"></i></a></li>\
-                  <li><a href="https://www.linkedin.com/in/paulsokolik" target="_blank"><i class="glyphicon icon-large icon-search icon-linked-in linkedin-icon"></i></a></li>\
-                  <li><a href="http://paulsokolik.com/" target="_blank"><i class="glyphicon icon-large icon-search icon-blog"></i></a></li>\
-                </ul></div>\
-            </div> \
-            <div class="member-profile col-xs-4" id="tamara-profile"> \
-                <img src="assets/images/Tamara.jpeg" class="profile-image">\
-                <div class="text-center row profile-name">Tamara Woodson</div>\
-                <div class="text-center row"><ul class="nav navbar navbar-nav about-us-nav">\
-                  <li><a href="https://github.com/tmwoodson" target="_blank"><i class="glyphicon icon-large icon-search icon-github"></i></a></li>\
-                  <li><a href="https://www.linkedin.com/pub/tamara-woodson/38/400/345" target="_blank"><i class="glyphicon icon-large icon-search icon-linked-in linkedin-icon"></i></a></li>\
-                  <li><a href="http://tmwoodson.github.io/" target="_blank"><i class="glyphicon icon-large icon-search icon-blog"></i></a></li>\
-                </ul></div>\
-            </div> \
           </div>\
         </div> \
      </div>\
